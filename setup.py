1dded3b8d7051d8f31327e6ab3a0eaab60856b6d
Kazunori Akiyama <kazu@haystack.mit.edu>
2018-01-04 16:07:23 -0500

    Update configure/make files.

--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
+if path.isfile("sparselab/libmfista.so"):
+    errmsg="Apparently, you have not compiled C/Fortran libraries with make."
+    errmsg+=" Please install this library by 'make install' not by 'python setup.py install'"
+    raise RuntimeError(errmsg)
+
 setup(
     name="sparselab",
     version = "0.0.1",
-    description = "a Library for Interferometric Imaging using Sparse Modeling",
+    description = "A Library for Interferometric Imaging and related analysis using Sparse Modeling",
     long_description = long_description,
-
     url = "https://eht-jp.github.io/sparselab",
     author = "Kazu Akiyama",
     author_email = "kakiyama@mit.edu",
     license = "MIT",
-
     keywords = "imaging astronomy EHT",
-
     packages = find_packages(exclude=["doc*", "test*"]),
-    install_requires = ["astropy", "matplotlib", "numpy", "pandas", "scikit-image", "scipy", "xarray"]
+    install_requires = [
+        "numpy","scipy","matplotlib","pandas","xarray",
+        "scikit-image", "astropy", "tqdm"]
 )
