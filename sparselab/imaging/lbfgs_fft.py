fix precision issues

sparselab/imaging/lbfgs_fft.py
@@ -32,7 +32,7 @@ from .. import util, imdata, fortlib
 # Default Parameters
 #-------------------------------------------------------------------------
 lbfgsbprms = {
-    "m": 10,
+    "m": 5,
     "factr": 1e1,
     "pgtol": 0.
 }
@@ -84,7 +84,7 @@ def imaging(
         print("log transform will be applied to regularization functions.")
         transtype = np.int32(1)
         if transprm is None:
-            transprm = np.finfo(np.float64).eps
+            transprm = 1e-10
         elif transprm <= 0:
             raise ValueError("transprm must be positive.")
         else:
@@ -232,8 +232,8 @@ def imaging(
             fluxscale = (fluxscale)**transprm
 
         lambl1_sim = lambl1 / (fluxscale * Nyx)
-        lambtv_sim = lambtv / (fluxscale * Nyx)
-        lambtsv_sim = lambtsv / (fluxscale**2 * Nyx)
+        lambtv_sim = lambtv / (4 * fluxscale * Nyx)
+        lambtsv_sim = lambtsv / (4 *fluxscale**2 * Nyx)
         lambmem_sim = lambmem / (fluxscale*np.log(fluxscale) * Nyx)
     else:
         lambl1_sim = lambl1

sparselab/imdata/imdata.py
@@ -696,7 +696,7 @@ class IMFITS(object):
     #-------------------------------------------------------------------------
     # Plotting
     #-------------------------------------------------------------------------
-    def imshow(self, logscale=False, angunit=None,
+    def imshow(self, logscale=False, angunit=None, dyrange=100,
                vmin=None, istokes=0, ifreq=0, **imshow_args):
         '''
         plot contours of the image
@@ -726,11 +726,11 @@ class IMFITS(object):
             imextent = self.get_imextent(angunit)
 
         if logscale:
-            plotdata = np.log(self.data[istokes, ifreq])
+            plotdata = np.log10(self.data[istokes, ifreq])
             if vmin is None:
-                vmin_scaled = np.log(0.001 * self.peak(istokes, ifreq))
+                vmin_scaled = np.log10(self.peak(istokes, ifreq)/dyrange)
             else:
-                vmin_scaled = np.log(vmin)
+                vmin_scaled = np.log10(vmin)
             plotdata[np.where(plotdata < vmin_scaled)] = vmin_scaled
             plt.imshow(plotdata, extent=imextent, origin="lower",
                        vmin=vmin_scaled, **imshow_args)