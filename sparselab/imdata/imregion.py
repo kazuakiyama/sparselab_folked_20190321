a99339d18ed3f973b89b57721fe1c87b09446ede
Kazunori Akiyama <kazu@haystack.mit.edu>
2018-02-12 21:21:22 -0500

    first commit of the imageregion submodule

---
+++ sparselab/imdata/imregion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+'''
+This is a sub-module of sparselab handling image fits data.
+'''
+__author__ = "Sparselab Developer Team"
+#-------------------------------------------------------------------------
+# Modules
+#-------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------
+# IMAGEFITS (Manupulating FITS FILES)
+#-------------------------------------------------------------------------
+class ImageRegion(object):
+    pass
