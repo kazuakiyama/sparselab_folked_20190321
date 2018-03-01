fa98131c2c95b6957d93810b585dde1b2089401d
Kazunori Akiyama <kazu@haystack.mit.edu>
2018-03-01 10:23:49 -0500

    add an exprimental version of new uvfits module

--- sparselab/uvdata/__init__.py
+++ sparselab/uvdata/__init__.py
@@ -15,3 +15,6 @@ from .uvfits  import UVFITS
 from .uvtable import VisTable, read_vistable
 from .uvtable import BSTable, read_bstable
 from .uvtable import CATable, read_catable
+
+# test module
+from . import uvfits_new
