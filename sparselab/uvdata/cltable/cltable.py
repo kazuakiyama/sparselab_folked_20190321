3922c246920c5d2838f94b28adb3da5c6b11b631
Fumie Tazaki <tazaki@FumienoMacBook-Pro.local>
2018-03-14 22:59:42 +0900

    mod cltable.py

--- sparselab/uvdata/cltable/cltable.py
+++ sparselab/uvdata/cltable/cltable.py
@@ -41,34 +41,42 @@ class CLTable(object):
     def __init__(self, uvfits):
         '''
         '''
+        self.gaintabs = {} #空dictionaryの作成
 
-        # ex. CLTable("m87simdata/jet.104.amperr.uvf")では
-        # uvfits = m87simdata/jet.104.amperr.uvfとなる
-        #print(uvfits)
-        #メモ uvfits = uvdata.uvfits.UVFITS("m87simdata/jet.104.uvf")
-
-        # get UTC
-        utc = np.datetime_as_string(uvfits.visdata.coord["utc"])
-        utc = sorted(set(utc)) # remove duplicated
-        self.utc = at.Time(utc, scale="utc") # バグを吐く
-        #print(uvfits.visdata.coord["utc"])
-
-    #def gain(self,uvfits):
-        # gainの配列の初期化
-        Ndata, Ndec, Nra, Nif, Nch, Nstokes, Ncomp=uvfits.visdata.data.shape
-        Ntime = len(self.utc)
-        # test Ntime
-        #Ntime =200
-
-        # Nantの計算 (uvfits.pyを参考にする)
+        # サブアレイの考慮
         subarrids = uvfits.subarrays.keys()
-        for subarrid in subarrids:    # arrayのidごとに異なる数のアレイの数
-                                      # 今回は一つのarrayのidを使うので、1で良いということか
-                                      # 一応、for文のまま残しておく
+        for subarrid in subarrids:
+
+            self.gaintabs[subarrid] = {} #空dictionaryの作成
+
+            # get UTC
+            utc = np.datetime_as_string(uvfits.visdata.coord["utc"])
+            utc = sorted(set(utc)) # remove duplicated
+            #print(utc)
+
+            utc = at.Time(utc, scale="utc")
+
+            # dictionaryの"utc"の名前にutcの値を格納
+            self.gaintabs[subarrid]["utc"]=utc
+            #print(self.utc)
+
+            #def gain(self,uvfits):
+            # gainの配列の初期化
+            Ndata, Ndec, Nra, Nif, Nch, Nstokes, Ncomp=uvfits.visdata.data.shape
+            Ntime = len(utc)
+            #print(Ntime)
+            #print(utc)
+            # test Ntime
+            #Ntime =200
+
+            # Nantの計算 (uvfits.pyを参考にする)
             arraydata = uvfits.subarrays[subarrid]
             Nant = arraydata.antable["name"].shape[0]
-        print("Ntime,Nif,Nch,Nstokes,Nant,3=")
-        print(Ntime,Nif,Nch,Nstokes,Nant,3)
-        self.gain = np.zeros([Ntime,Nif,Nch,Nstokes,Nant,3])
-        #gain = np.zeros([Ntime,Nif,Nch,Nstokes,Nant,3])
-    #    return gain
+            #print("Ntime,Nif,Nch,Nstokes,Nant,3(=greal,gimag,sigma)=")
+            #print(Ntime,Nif,Nch,Nstokes,Nant,3)
+            #self.gain = np.zeros([Ntime,Nif,Nch,Nstokes,Nant,3])
+
+            gain = np.zeros([Ntime,Nif,Nch,Nstokes,Nant,3])
+            gain[:,:,:,:,:,0]=1
+            # dictionaryの"gain"にgainを格納
+            self.gaintabs[subarrid]["gain"]=gain
